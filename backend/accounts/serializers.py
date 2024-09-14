35aa7e01b746bb290c2ee0164b1c3622077fbb4f
Erik Polzin <eriktpol@gmail.com>
2024-09-14 16:03:27 +0200

    Add bytes sent/recv in user profile

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
+from django.db.models import Sum
 from rest_framework import serializers
 
 from .models import UserProfile
+from radius.models import Radacct
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -13,6 +15,26 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = UserProfile
         fields = "__all__"
         read_only_fields = ("user",)
+
+    num_sessions = serializers.SerializerMethodField()
+    bytes_recv = serializers.SerializerMethodField()
+    bytes_sent = serializers.SerializerMethodField()
+
+    def sessions(self, user: User):
+        return Radacct.objects.filter(username=user.username)
+
+    def get_num_sessions(self, profile: UserProfile) -> int:
+        return self.sessions(profile.user).count()
+
+    def get_bytes_recv(self, profile: UserProfile) -> int:
+        return self.sessions(profile.user).aggregate(Sum("acctinputoctets"))[
+            "acctinputoctets__sum"
+        ]
+
+    def get_bytes_sent(self, profile: UserProfile) -> int:
+        return self.sessions(profile.user).aggregate(Sum("acctoutputoctets"))[
+            "acctoutputoctets__sum"
+        ]
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -30,7 +52,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """Update profile after creating the user."""
-        profile_data = validated_data.pop('profile', None)
+        profile_data = validated_data.pop("profile", None)
         user = super().create(validated_data)
         if profile_data:
             profile_serializer = UserProfileSerializer(user.profile, data=profile_data)
